ddcfb2f9945fd1d4902a956210759cd99b47024c
daregoodness1997 <daregoodness@gmail.com>
2023-09-28 22:37:45 +0100

    update design

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,48 +1,31 @@
-let spinnerWrapper = document.querySelector(".spinner-wrapper");
+const textSlide = document.querySelector(".text-slide");
+const text = textSlide.querySelectorAll("p");
 
-window.addEventListener("load", () => {
-  //   spinnerWrapper.style.display = "none";
-  spinnerWrapper.parentElement.removeChild(spinnerWrapper);
+let counter = 1;
+const size = text[0].clientWidth;
+
+//to set the slide one as the first slide instead of the clone
+textSlide.style.transform = `translateX(-${size * counter}px)`;
+
+//looping the slider every 2s
+setInterval(() => {
+  if (counter >= text.length - 1) return;
+
+  textSlide.style.transition = `transform 0.8s ease`;
+  counter++;
+  textSlide.style.transform = `translateX(-${size * counter}px)`;
+}, 2000);
+
+textSlide.addEventListener("transitionend", () => {
+  if (text[counter].id === "lastClone") {
+    textSlide.style.transition = `none`;
+    counter = text.length - 2;
+    textSlide.style.transform = `translateX(-${size * counter}px)`;
+  }
+
+  if (text[counter].id === "firstClone") {
+    textSlide.style.transition = `none`;
+    counter = text.length - counter;
+    textSlide.style.transform = `translateX(-${size * counter}px)`;
+  }
 });
-
-const openBtn = document.querySelector(".open");
-const closeBtn = document.querySelector(".close");
-const navLinks = document.querySelector(".nav-links");
-
-openBtn.addEventListener("click", () => {
-  navLinks.classList.add("show");
-  closeBtn.classList.toggle("hide");
-  console.log("test", navLinks);
-});
-closeBtn.addEventListener("click", () => {
-  navLinks.classList.remove("show");
-  closeBtn.classList.toggle("hide");
-});
-
-// Scroll Reveal Animation
-const sr = ScrollReveal({
-  origin: "top",
-  distance: "80px",
-  duration: 2000,
-  reset: true,
-});
-
-// Scroll Home
-
-// Navbar
-sr.reveal(".logo", { delay: 500 });
-sr.reveal(".nav-link", { delay: 500, interval: 200 });
-
-// Banner
-sr.reveal(".title-head", { origin: "right", delay: 400 });
-sr.reveal(".title-text", { origin: "left", delay: 600 });
-sr.reveal(".banner ", { origin: "right", delay: 600 });
-// sr.reveal(".banner img", { origin: "right", delay: 600, interval: 500 });
-sr.reveal(".quote", { delay: 300 });
-
-// Reveal
-sr.reveal(".container ", { origin: "top" });
-sr.reveal(".container .order1", { origin: "left", delay: 300 });
-sr.reveal(".container .order2", { origin: "right", delay: 600 });
-sr.reveal(".right", { origin: "right", delay: 400 });
-sr.reveal(".left", { origin: "left", delay: 600 });
